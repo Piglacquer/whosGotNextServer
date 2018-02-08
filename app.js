59df290b71cba4ec0f1c826686f4d2427ca19556
Patrick Biffle <pbiffle@gmail.com>
2018-02-08 09:49:53 -0700

    fucking cors again

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 app.use(bodyParser.json())
-app.use(cors)
+app.use(cors())
 // app.use(function(req, res, next) {
 // 	res.header('Access-Control-Allow-Origin', '*')
 // 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
