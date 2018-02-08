154db47e39de9157192ebc536d8e0a8b1559a09b
Patrick Biffle <pbiffle@gmail.com>
2018-02-08 10:01:15 -0700

    shit ass json

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(cors())
-// app.use(function(req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', '*')
-// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-// 	res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE')
-// 	next()
-// })
 
 app.get('/', (request, response) => {
 	queries
@@ -34,6 +28,7 @@ app.post('/', (request, response) => {
 app.delete('/:id', (request, response) => {
 	queries
 		.removePlayer(request.params.id)
+		.then(response => response.json())
 		.then(() => {
 			response.sendStatus(200)
 		})
