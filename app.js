81c425c38d98e827dea0cc6e7b884568e63b85f2
Patrick Biffle <pbiffle@gmail.com>
2018-02-08 09:47:11 -0700

    added headers for cors

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 app.use(bodyParser.json())
-// app.use(function(req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', '*')
-// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-// 	res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE')
-// 	next()
-// })
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE')
+	next()
+})
 
 app.get('/', (request, response) => {
 	queries
