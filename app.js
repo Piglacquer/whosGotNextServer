2712fe75e52fd1676bc808bc7ea1bf3bd985c1ae
Patrick Biffle <pbiffle@gmail.com>
2018-02-08 09:49:22 -0700

    god damn cors

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 app.use(bodyParser.json())
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*')
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-	res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE')
-	next()
-})
+app.use(cors)
+// app.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*')
+// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+// 	res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE')
+// 	next()
+// })
 
 app.get('/', (request, response) => {
 	queries
